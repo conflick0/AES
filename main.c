94ff46a4714ab35962be24118accabb4f3b33935
conflick0 <jpjhsu@gms.tku.edu.tw>
2019-12-02 19:35:33 +0800

    complete base encryption,and key expansion 128bit

--- main.c
+++ main.c
@@ -8,9 +8,17 @@ unsigned int *Decryption(unsigned int *state, unsigned int *exp_key, int round);
 int main(void) {
     int i;
     int round = 10;
-    unsigned char inp_16_bytes[16] = "0000000000000000";
+    unsigned char inp_16_bytes[16] = {0x32, 0x43, 0xf6, 0xa8,
+                                      0x88, 0x5a, 0x30, 0x8d,
+                                      0x31, 0x31, 0x98, 0xa2,
+                                      0xe0, 0x37, 0x07, 0x34};//3243f6a8885a308d313198a2e0370734
 
-    char inp_key[16] = "0000000000000000";
+    unsigned char inp_key[16] = {0x2b, 0x7e, 0x15, 0x16,
+                                 0x28, 0xae, 0xd2, 0xa6,
+                                 0xab, 0xf7, 0x15, 0x88,
+                                 0x09, 0xcf, 0x4f, 0x3c};//2b7e151628aed2a6abf7158809cf4f3c
+
+
     unsigned int *state;
     unsigned int *exp_key;
 
@@ -36,40 +44,13 @@ int main(void) {
     printf("Initial state:\n");
     PrintState(state);
 
-    /* Initial round add round key  */
-    state = AddRoundKey(state, exp_key, 0);
-    printf("Add key0:\n");
-    PrintState(state);
-
-    printf("\nr %d:\n",0);
-    state = SubBytes(state);
-    printf("subbyte\n");
-    PrintState(state);
-    printf("\n");
-
-    state = ShiftRow(state);
-    printf("shift\n");
-    PrintState(state);
-    printf("\n");
-
-    state = MixColumns(state);
-    printf("mix\n");
-    PrintState(state);
-    printf("\n");
-
-    state = AddRoundKey(state, exp_key, 1);
-    printf("a\n");
-    PrintState(state);
-    printf("\n");
-
-
     state = Encryption(state, exp_key, round);
     printf("Encryption state:\n");
     PrintState(state);
 
-    state = Decryption(state, exp_key, round);
-    printf("Decryption state:\n");
-    PrintState(state);
+//    state = Decryption(state, exp_key, round);
+//    printf("Decryption state:\n");
+//    PrintState(state);
 
     free(state);
     free(exp_key);
